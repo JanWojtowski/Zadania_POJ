97f3c0af03b89d1c0bf7b5e0c07f5caeb128eb61
JanWojtowski <s26978@pjwstk.edu.pl>
2023-03-06 17:18:05 +0100

    Lab2 zad 1

--- Lab2/Zadanie_1.java
+++ Lab2/Zadanie_1.java
@@ -1,4 +1,24 @@
 package Lab2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Zadanie_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj wielkość tablicy:");
+        int size = scanner.nextInt();
+
+        String[] tab = new String[size];
+
+        for(int i = 0; i < size; i++ ){
+            System.out.println("Podaj znaki które mają znaleść sie na pozycji " + (i+1) + " tablicy:");
+            tab[i] = scanner.next();
+        }
+
+        System.out.println("Wygenerowana tablica to: " + Arrays.toString(tab));
+    }
+
 }
