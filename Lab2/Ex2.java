0e5002dd4520adf48c19241d01c71f59f75b5e19
JanWojtowski <120735941+JanWojtowski@users.noreply.github.com>
2023-03-08 12:51:00 +0100

    Ex2

--- Lab2/Ex2.java
+++ Lab2/Ex2.java
@@ -14,6 +14,13 @@ public class Ex2 {
         System.out.println("Podaj pierwszą pozycje: ");
         int first = scanner.nextInt();
 
+        int[] tab = bulidTab(size,first);
+
+        statsOfTab(tab);
+    }
+
+    public static int[] bulidTab(int size, int first){
+
         int[] tab = new int[size];
 
         tab[0] = first;
@@ -35,11 +42,74 @@ public class Ex2 {
             }
         }
 
-        System.out.println(Arrays.toString(tab));
-
+        return tab;
     }
 
+    public static int minInTab(int[] tab){
+        int min = tab[0];
 
+        for (int j : tab) {
+            if (min > j) {
+                min = j;
+            }
+        }
 
+        return min;
+    }
 
+    public static int maxInTab(int[] tab){
+        int max = tab[0];
+
+        for(int i : tab){
+            if(max < i){
+                max = i;
+            }
+        }
+
+        return max;
+    }
+
+    public static int sumOfTab(int[] tab){
+        int sum = 0;
+
+        for(int i : tab){
+            sum += i;
+        }
+
+        return sum;
+    }
+
+    public static double averageTab(int[] tab){
+        int sum = 0;
+
+        for(int i : tab){
+            sum += i;
+        }
+
+        return (sum/(double)(tab.length));
+    }
+
+    public static double medianTab(int[] tab){
+        double med;
+
+        if(tab.length % 2 == 1){
+            med = tab[(tab.length / 2)];
+        }
+        else {
+            med = (tab[(tab.length / 2)-1] + tab[(tab.length / 2)]) / 2.0;
+        }
+
+        return med;
+    }
+
+    public static void statsOfTab(int[] tab){
+        System.out.println("Powstała tablica:");
+        System.out.println(Arrays.toString(tab));
+        System.out.println("Statystyki:");
+        System.out.println("Wartość minimalna: " + minInTab(tab));
+        System.out.println("Wartość maksymalna: " + maxInTab(tab));
+        System.out.println("Suma wartość: " + sumOfTab(tab));
+        System.out.println("Średnia wartość: " + averageTab(tab));
+        System.out.println("Mediana wartość: " + medianTab(tab));
+    }
 }
